0b9bbd1afddbef23f3d3d85af36fb1b08803d71e
Imran189 <imranimu189@gmail.com>
2022-04-08 10:27:29 +0600

    Html To Vue Convertion

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import AboutViewVue from '../views/AboutView.vue'
 const routes = [
   {
     path: '/',
@@ -10,10 +10,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutViewVue
   }
 ]
 
