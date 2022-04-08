002acd2f77e42a5c240043119e96a8976b1a678b
Imran189 <imranimu189@gmail.com>
2022-04-08 10:49:24 +0600

    1st commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/pages/MasterView.vue'
 import AboutViewVue from '../views/AboutView.vue'
 const routes = [
   {
